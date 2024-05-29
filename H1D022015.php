a73817408537c13511322740d48591aecce8f384
auradvny <auradvny14@gmail.com>
2024-05-29 15:11:41 +0700

    Menambahkan file H1D022015.php

---
+++ H1D022015.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan</title>
+</head>
+
+<body>
+    <h1>Halo! </h1>
+    <p>Nama : Aura Devany Salsabila Bachtiar</p>
+    <p>NIM : H1D022015</p>
+</body>
+
+</html>
